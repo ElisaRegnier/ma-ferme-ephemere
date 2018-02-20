1e9a85006ab1898af5c7f45312d36aaade343d5f
Elliot Tyson <tison.elliot@gmail.com>
2018-02-20 15:19:26 +0100

    controller bookings and users controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,24 +3,46 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def update
+  def accept
     @booking = Booking.find(params[:id])
-
-
-
+    @booking.accepted = "Accepté"
     @booking.save
     redirect_to booking_path(@booking)
   end
 
-  def destroy
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.accepted = "Refusé"
+    @booking.save
+    redirect_to booking_path(@booking)
   end
+
+  def new
+    @farm = Farm.find(params[:farm_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.farm = Farm.find(params[:farm_id])
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to farms_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :number_of_people)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookings/create'
-
-  get 'bookings/new'
-
-  get 'bookings/edit'
-
-  get 'bookings/show'
-
-  get 'bookings/update'
-
-  get 'bookings/destroy'
-
   devise_for :users
   root to: 'pages#home'
   resources :farms do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [ :show, :edit, :update, :destroy ]
+  resources :bookings, only: [ :show, :destroy ]
+  get 'bookings/:id/accept', to: 'bookings#accept', as: :accept
+  get 'bookings/:id/decline', to: 'bookings#decline', as: :decline
+  get 'bookings', to: 'users#bookings', as: :bookings
 end
