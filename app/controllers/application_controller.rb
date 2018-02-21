6e39c0c7fac1e33af133dc8ce6128383d1e65ce2
Elisa Regnier <regnier.elisa@gmail.com>
2018-02-21 12:27:43 +0100

    profil show & avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,10 +19,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo])
   end
 
   private
