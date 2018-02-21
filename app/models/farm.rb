4f3585591a0a61856059f59c476e6c73f84f83f1
Elliot Tyson <tison.elliot@gmail.com>
2018-02-21 12:18:21 +0100

    with map and markers

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,6 @@ class Farm < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
